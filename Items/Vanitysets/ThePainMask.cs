a5e1f70eebb4cc6abbbcca9705f83df224ef9c38
StormDiver <64652305+StormDiver@users.noreply.github.com>
2022-05-19 14:53:03 +0100

    1.9.1.3

--- Items/Vanitysets/ThePainMask.cs
+++ Items/Vanitysets/ThePainMask.cs
@@ -49,6 +49,47 @@ namespace StormDiversSuggestions.Items.Vanitysets
         }
 
     }
+    [AutoloadEquip(EquipType.Head)]
+    public class TheClaymanMask : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            base.SetStaticDefaults();
+            DisplayName.SetDefault("Clayman");
+            Tooltip.SetDefault("Sliently judge everybody around you");
+        }
 
+        public override void SetDefaults()
+        {
+            item.width = 18;
+            item.height = 18;
+            item.value = Item.sellPrice(0, 1, 0, 0);
+            item.rare = ItemRarityID.White;
+            item.vanity = true;
+
+        }
+
+
+        public override void ArmorSetShadows(Player player)
+        {
+
+        }
+
+
+        public class ModGlobalNPC : GlobalNPC
+        {
+            public override void NPCLoot(NPC npc)
+            {
+
+                if (Main.rand.Next(50) < 1 && npc.boss)
+
+                {
+                    Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("TheClaymanMask"));
+                }
+
+            }
+        }
+
+    }
 
 }
